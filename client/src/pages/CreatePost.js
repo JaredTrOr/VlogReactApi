89e9e338ee037fc4f616f8549e47dd7ed9dd0f58
JaredTrOr <trujillojaredalexander@gmail.com>
2023-01-15 12:17:51 -0600

    It was implemented the post operation and validation

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,8 +2,46 @@ import {Link} from 'react-router-dom';
 import {Formik, Form, Field, ErrorMessage} from 'formik'
 import '../styles/CreatePost.css';
 import writePostImg from '../images/write-your-post.png';
+import * as Yup from 'yup';
+import axios from 'axios';
+import {useState} from 'react';
 
 function CreatePost() {
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const initialValues = {
+    username: '',
+    title: '',
+    postText: ''
+  };
+
+  const validationScheme = Yup.object().shape({
+    username: Yup.string().required(),
+    title: Yup.string().required(),
+    postText: Yup.string()
+  });
+
+  const onSubmitPost = async (data) => {
+    const {username, title, postText} = data;
+
+    try{
+      await axios.post('http://localhost:3000/posts', {
+        title, 
+        postText,
+        username
+      });
+      console.log('Post created');
+      setSuccessMessage('The post was created succesfully!!');
+      setTimeout(() => {
+        setSuccessMessage('');
+      },2000);
+    }
+    catch(err){
+      console.log(err);
+    }
+    
+  }
+
   return (
     <div className="main-container">
       <div className='create-post-nav'>
@@ -13,19 +51,34 @@ function CreatePost() {
 
       <div className='create-post-row'>
         <div className='create-post-column'>
-          <Formik>
+          <Formik 
+            initialValues={initialValues} 
+            onSubmit={onSubmitPost}
+            validationSchema={validationScheme}
+            >
+
             <Form className='form-post'>
               <div className='input-post'>
-                <label for='title'>Username:</label>
+                <label htmlFor='username'>Username:</label>
+                <ErrorMessage 
+                  name='username' 
+                  component='span'
+                  className='error-message'
+                />
                 <Field 
                   className='input-create-post' 
-                  name='title' 
+                  name='username' 
                   placeholder='(Ex. Username...)'
                 />
               </div>
               
               <div className='input-post'>
-                <label for='title'>Title:</label>
+                <label htmlFor='title'>Title:</label>
+                <ErrorMessage 
+                  name='title' 
+                  component='span'
+                  className='error-message'
+                />
                 <Field 
                   className='input-create-post' 
                   name='title' 
@@ -34,10 +87,11 @@ function CreatePost() {
               </div>
               
               <div className='input-post'>
-                <label for='title'>Post text:</label>
+                <label htmlFor='post-text'>Post text:</label>
+                <ErrorMessage name='post-text' component='span'/>
                 <Field 
-                  className='input-create-post input-post-text' 
-                  name='title' 
+                  className='input-create-post' 
+                  name='postText' 
                   placeholder='(Ex. Text...)'
                 />
               </div>
@@ -45,6 +99,8 @@ function CreatePost() {
               <div>
                 <button className='submit-post' type='submit'>Create post</button>
               </div>
+
+              <div>{successMessage}</div>
             </Form>
           </Formik>
         </div>
